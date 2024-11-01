5a783cb9d08a0c56288882c76450c90e8344449a
CristianGarcia19 <cristiangarcia1914@hotmail.com>
2024-11-01 18:25:02 -0500

    Actualizacion de la conexion de la bd y configuracion de rutas

--- models/Conexion.php
+++ models/Conexion.php
@@ -2,12 +2,13 @@
     $host = 'localhost';
     $bdname = 'inmobiliaria';
     $username = 'root';
-    $password = 'cristian1904';
+    $password = '';
+    $port = '3308';
 
-    $conexion = mysqli_connect($host, $username, $password, $bdname);
+    $conexion = mysqli_connect($host, $username, $password, $bdname, $port);
 
     if(!$conexion){
-        die("Error en la conexión: " . mysqli_connect_error());
+        die("Error en la conexión: " . mysqli_connect_error());//valida si hay algo mal en la conexion
     }
-    echo "Conexion exitosa";
+    /*echo "Conexion exitosa";*/
 ?>
